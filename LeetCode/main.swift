1f0b007ce5f9b77c29439b14d21376cbf4ca722d
Toru Asai <acaibowl.dev@gmail.com>
2019-04-01 23:28:44 +0900

    Initial Commit

---
+++ LeetCode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LeetCode
+//
+//  Created by Toru Asai on 2019/04/01.
+//  Copyright © 2019 Toru Asai. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
