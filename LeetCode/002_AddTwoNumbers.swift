2ed9bc579909662ef4f85eb7e7e7ddce88374a35
tasaiii725 <tasaiii725@gmail.com>
2019-04-23 13:54:06 +0900

    Update 002_AddTwoNumbers.swift

--- LeetCode/002_AddTwoNumbers.swift
+++ LeetCode/002_AddTwoNumbers.swift
@@ -32,8 +32,28 @@ class AddTwoNumbersSolution {
             self.next = nil
         }
     }
-    
     func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
-        return nil
+        let dummy = ListNode(0)
+        var l1 = l1, l2 = l2, carry = 0
+        var node: ListNode? = dummy
+        
+        while l1 != nil || l2 != nil {
+            let x = l1?.val ?? 0
+            let y = l2?.val ?? 0
+            let sum = carry + x + y
+            carry = sum / 10
+            node?.next = ListNode(sum % 10)
+            node = node?.next
+            if l1 != nil {
+                l1 = l1?.next
+            }
+            if l2 != nil {
+                l2 = l2?.next
+            }
+        }
+        if carry > 0 {
+            node?.next = ListNode(carry)
+        }
+        return dummy.next // because ListNode is class
     }
 }
