959aa03cf718e9899db44d97761c11eb52b9f41c
tasaiii725 <tasaiii725@gmail.com>
2019-04-24 13:53:26 +0900

    Update 003_LongestSubstringWithoutRepeatingCharacters.swift
    WIP

--- LeetCode/003_LongestSubstringWithoutRepeatingCharacters.swift
+++ LeetCode/003_LongestSubstringWithoutRepeatingCharacters.swift
@@ -34,7 +34,50 @@
 import Foundation
 
 class LengthOfLongestSubstringSolution {
+//    func lengthOfLongestSubstring(_ s: String) -> Int {
+//        var result: Int = 0
+//
+//        var temp: [Character] = []
+//        for (i1, c1) in s.enumerated() {
+//            temp.append(c1)
+//            for i2 in (i1+1)..<s.count {
+//                let c = s[s.index(s.startIndex, offsetBy: i2)]
+//                if !temp.contains(c) {
+//                    temp.append(c)
+//                } else {
+//                    break
+//                }
+//            }
+//            if result < temp.count {
+//                result = temp.count
+//            }
+//            temp.removeAll()
+//        }
+//
+//        return result
+//    }
+    
     func lengthOfLongestSubstring(_ s: String) -> Int {
-        return 0
+        var result: Int = 0
+        
+        var temp: [Character] = []
+        for (i1, c1) in s.enumerated() {
+            temp.append(c1)
+            for i2 in (i1+1)..<s.count {
+                let c = s[s.index(s.startIndex, offsetBy: i2)]
+                if !temp.contains(c) {
+                    temp.append(c)
+                } else {
+                    break
+                }
+            }
+            if result < temp.count {
+                result = temp.count
+            }
+            temp.removeAll()
+        }
+        
+        return result
     }
+
 }
